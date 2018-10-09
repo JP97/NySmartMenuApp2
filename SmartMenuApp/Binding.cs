9b29d70bb21893cbf449d07bfa3d683db95c9785
Julian <juli11c0@edu.eal.dk>
2018-10-09 18:02:31 +0200

    skrevet noget i bindings Class( håber det er rigtigt)

--- SmartMenuApp/Binding.cs
+++ SmartMenuApp/Binding.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace SmartMenuApp
 {
-    class Binding
+    public class Binding
     {
-    }
+        static void SwitchCase(int caseSwitch)
+        {
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine("du har valgt nr. 1");
+                    Console.ReadLine();
+                    break;
+
+                case 2:
+                    Console.WriteLine("du har valgt nummer 2");
+                    Console.ReadLine();
+                    break;
+
+                case 3:
+                    Console.WriteLine("du har valgt nummer 3");
+                    Console.ReadLine();
+                    break;
+
+                default:
+                    break;
+
+            }
+        }
+        
+}
 }
