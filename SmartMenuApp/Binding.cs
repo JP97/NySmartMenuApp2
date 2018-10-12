fa0d337059cd3a560f1704b6fb4f58151f819a4c
Julian <juli11c0@edu.eal.dk>
2018-10-12 09:25:52 +0200

    Arbejdet i binding Class

--- SmartMenuApp/Binding.cs
+++ SmartMenuApp/Binding.cs
@@ -3,28 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CalculatorLibrary;
 
 namespace SmartMenuApp
 {
     public class Binding
     {
-        static void SwitchCase(int caseSwitch)
+        public static void SwitchCase(int caseSwitch)
         {
             switch (caseSwitch)
             {
                 case 1:
-                    Console.WriteLine("du har valgt nr. 1");
-                    Console.ReadLine();
+
                     break;
 
                 case 2:
-                    Console.WriteLine("du har valgt nummer 2");
-                    Console.ReadLine();
+
                     break;
 
                 case 3:
-                    Console.WriteLine("du har valgt nummer 3");
-                    Console.ReadLine();
+                    
+                   double a = double.Parse(Console.ReadLine());
+                   double b = double.Parse(Console.ReadLine());
+              
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    break;
+
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+
+                case 8:
+
                     break;
 
                 default:
